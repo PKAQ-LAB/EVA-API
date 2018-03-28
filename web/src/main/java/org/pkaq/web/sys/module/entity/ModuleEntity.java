812caac43ad9afdc37b9b8171acd238093d20c71
PKAQ <pkaq@msn.com>
2018-03-28 22:15:31 +0800

    模块管理相关代码

---
+++ web/src/main/java/org/pkaq/web/sys/module/entity/ModuleEntity.java
@@ -0,0 +1,57 @@
+package org.pkaq.web.sys.module.entity;
+
+import com.baomidou.mybatisplus.annotations.TableField;
+import com.baomidou.mybatisplus.annotations.TableName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.apache.ibatis.type.Alias;
+import org.pkaq.core.mvc.entity.BaseEntity;
+import org.pkaq.web.sys.dict.entity.DictEntity;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * 模块管理
+ * @author : PKAQ
+ */
+@Data
+@Alias("module")
+@TableName("sys_module")
+@EqualsAndHashCode(callSuper = true)
+public class ModuleEntity extends BaseEntity implements Serializable {
+    private String name;
+
+    private String icon;
+
+    private String routeurl;
+
+    private String modelurl;
+
+    private String code;
+
+    private String parentid;
+
+    private String parentname;
+
+    private String path;
+
+    private String pathname;
+
+    private Boolean isLeaf;
+
+    private Integer orders;
+
+    private String status;
+
+    private static final long serialVersionUID = 1L;
+
+    /** 子节点 **/
+    @TableField(exist = false)
+    private List<DictEntity> children;
+
+    public List<DictEntity> getChildren() {
+        return children == null || children.size()<1?null:children;
+    }
+
+}
