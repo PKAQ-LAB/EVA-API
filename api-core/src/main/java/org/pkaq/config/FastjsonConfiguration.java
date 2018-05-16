de817b06d631ef70c2efff927814791fa9b21533
PKAQ <pkaq@msn.com>
2018-05-16 22:43:37 +0800

    fix #IJRSL 模块管理-列表-树节点加号显示错误，无子节点的叶子节点不应显示为+

--- api-core/src/main/java/org/pkaq/config/FastjsonConfiguration.java
+++ api-core/src/main/java/org/pkaq/config/FastjsonConfiguration.java
@@ -1,7 +1,9 @@
 package org.pkaq.config;
 
+import cn.hutool.core.collection.CollectionUtil;
 import cn.hutool.core.date.DatePattern;
 import cn.hutool.core.date.DateUtil;
+import com.alibaba.fastjson.serializer.PropertyFilter;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
@@ -9,6 +11,8 @@ import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+
+import java.util.List;
 
 
 /**
@@ -27,6 +31,8 @@ public class FastjsonConfiguration {
         // 格式化json,
         fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
         fastJsonConfig.setDateFormat(DatePattern.NORM_DATETIME_PATTERN);
+        fastJsonConfig.setSerializeFilters((PropertyFilter) (object, name, value) ->
+                !(value instanceof List) || ((List) value).size() >= 1);
         //在转换器中添加配置信息
         fastConverter.setFastJsonConfig(fastJsonConfig);
 
