7c98b3c8b307adead78b96fb8085b5fdd2b7e52f
PKAQ <pkaq@msn.com>
2018-08-10 23:07:35 +0800

    针对模块个性化配置swagger

---
+++ api-jxc/src/main/java/org/pkaq/config/SwaggerConfiguration.java
@@ -0,0 +1,45 @@
+package org.pkaq.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * swagger配置类
+ * Datetime: 2016-11-25 11:44
+ * @author PKAQ
+ * @Profile()
+ */
+@Configuration
+@EnableSwagger2
+@Slf4j
+public class SwaggerConfiguration {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("org.pkaq"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("进销存模块API - JXC RESTful APIs")
+                .description("http://pkaq.org")
+                .termsOfServiceUrl("http://pkaq.org/")
+                .contact(new Contact("pkaq","http://pkaq.org","pkaq@msn.com"))
+                .version("1.0")
+                .build();
+    }
+
+}
