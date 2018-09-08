9234f1842c82bfc231b8ea08e21830b700d93b3b
PKAQ <pkaq@msn.com>
2018-09-08 23:32:51 +0800

    close 使用P6SpyDriver来监控sql执行 #IMONA

---
+++ api-core/src/main/java/org/pkaq/core/log/P6SpyLogger.java
@@ -0,0 +1,19 @@
+package org.pkaq.core.log;
+
+import com.p6spy.engine.spy.appender.MessageFormattingStrategy;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author: S.PKAQ
+ * @Datetime: 2018/9/8 22:51
+ */
+public class P6SpyLogger implements MessageFormattingStrategy {
+
+    @Override
+    public String formatMessage(int connectionId, String now, long elapsed, String category, String prepared, String sql) {
+        return !"".equals(sql.trim()) ? "[ " + LocalDateTime.now() + " ] --- | took "
+                + elapsed + "ms | " + category + " | connection " + connectionId + "\n "
+                + sql + ";" : "";
+    }
+}
