644e03ecfe9d323b7d8c0cc750d4675e3d14e124
PKAQ <pkaq@msn.com>
2018-11-07 21:34:52 +0800

    接口鉴权支持

--- api-sys/src/main/java/org/pkaq/config/SwaggerConfiguration.java
+++ api-sys/src/main/java/org/pkaq/config/SwaggerConfiguration.java
@@ -1,5 +1,6 @@
 package org.pkaq.config;
 
+import io.swagger.models.auth.SecuritySchemeDefinition;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,10 +8,14 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
 
 /**
  * swagger配置类
@@ -25,6 +30,8 @@ public class SwaggerConfiguration {
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .securitySchemes(List.of(
+                        new ApiKey("Authorization", "Authorization", "header")))
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("org.pkaq"))
