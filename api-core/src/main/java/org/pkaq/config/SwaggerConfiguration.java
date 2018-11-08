c743bc126308375e22fba08f9acbcfed516a7f0d
PKAQ <pkaq@msn.com>
2018-11-08 20:39:07 +0800

    add jxc xmind

--- api-core/src/main/java/org/pkaq/config/SwaggerConfiguration.java
+++ api-core/src/main/java/org/pkaq/config/SwaggerConfiguration.java
@@ -1,16 +1,20 @@
 package org.pkaq.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
 
 /**
  * swagger配置类
@@ -21,10 +25,13 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 @Slf4j
+@ConditionalOnProperty(prefix = "spring.profiles", name = "active", havingValue = "dev")
 public class SwaggerConfiguration {
     @Bean
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .securitySchemes(List.of(
+                        new ApiKey("Authorization", "Authorization", "header")))
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("org.pkaq"))
@@ -34,7 +41,7 @@ public class SwaggerConfiguration {
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("进销存模块API - JXC RESTful APIs")
+                .title("RESTful APIs")
                 .description("http://pkaq.org")
                 .termsOfServiceUrl("http://pkaq.org/")
                 .contact(new Contact("pkaq","http://pkaq.org","pkaq@msn.com"))
