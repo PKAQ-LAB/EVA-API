593aa3f69992e807a6870c898a864304046a5f1a
PKAQ <pkaq@msn.com>
2018-12-25 09:45:11 +0800

    fix #3 部门管理 排序设置无效

--- api-core/src/main/resources/db/migration/V2__Add_new_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `log_biz`  (
-	`ID`             	varchar(40) NOT NULL,
-	`operator`       	varchar(10) NULL,
-	`operate_type`    	varchar(10) NULL,
-	`operate_dateTime`	varchar(20) NULL,
-	`description`    	varchar(300) NULL,
-	PRIMARY KEY(`ID`)
-)
