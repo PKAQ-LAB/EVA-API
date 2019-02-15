4b5bf9de14c9d80457f4d32424d6336979ca4462
PKAQ <pkaq@msn.com>
2019-02-15 13:06:01 +0800

    优化swagger配置

---
+++ api-web/src/main/java/io/nerv/config/SwaggerConfiguration.java
@@ -0,0 +1,49 @@
+package io.nerv.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
+
+/**
+ * swagger配置类
+ * Datetime: 2016-11-25 11:44
+ * @author PKAQ
+ * @Profile()
+ */
+@Configuration
+@EnableSwagger2
+@ConditionalOnProperty(prefix = "spring.profiles", name = "active", havingValue = "dev")
+public class SwaggerConfiguration {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .securitySchemes(List.of(
+                        new ApiKey("Authorization", "Authorization", "header")))
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("io.nerv"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("RESTful APIs")
+                .description("http://pkaq.org")
+                .termsOfServiceUrl("http://pkaq.org/")
+                .contact(new Contact("pkaq","http://pkaq.org","pkaq@msn.com"))
+                .version("1.0")
+                .build();
+    }
+}
