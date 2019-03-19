f992f60ba6e46506b5b52c63ee8553b93151ee49
PKAQ <pkaq@msn.com>
2019-03-19 21:54:06 +0800

    TreeSelectVo

---
+++ api-core/src/main/java/io/nerv/core/mvc/vo/TreeSelectVo.java
@@ -0,0 +1,25 @@
+package io.nerv.core.mvc.vo;
+
+/**
+ * antd treeSelect需要的数据结构
+ * @author: S.PKAQ
+ * @Datetime: 2019/3/19 21:45
+ */
+public class TreeSelectVo {
+    /** 全树唯一 **/
+    private String key;
+    /** 显示名称 **/
+    private String title;
+    /** 节点值 **/
+    private String value;
+    /** 是否叶子 **/
+    private String isLeaf;
+    /** 路径 **/
+    private String path;
+    /** 路径中文描述 **/
+    private String pathName;
+    /** 上级节点id **/
+    private String parentId;
+    /** 上级节点名称 **/
+    private String parentName;
+}
