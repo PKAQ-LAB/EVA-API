585f50b169b5f82ddb6d52c2bf06037834f3531e
Adameve <1135656594@qq.com>
2019-03-19 15:14:25 +0800

    增加修改删除字典项时刷新字典缓存

--- api-sys/src/main/java/io/nerv/web/sys/dict/ctrl/DictItemCtrl.java
+++ api-sys/src/main/java/io/nerv/web/sys/dict/ctrl/DictItemCtrl.java
@@ -3,10 +3,12 @@ package io.nerv.web.sys.dict.ctrl;
 import io.nerv.core.mvc.ctrl.PureBaseCtrl;
 import io.nerv.core.mvc.util.Response;
 import io.nerv.web.sys.dict.entity.DictItemEntity;
+import io.nerv.web.sys.dict.helper.DictHelperProvider;
 import io.nerv.web.sys.dict.service.DictItemService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,12 +27,15 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/dictItem")
 public class DictItemCtrl extends PureBaseCtrl<DictItemService> {
+    @Autowired
+    private DictHelperProvider dictHelper;
 
     @GetMapping("/del/{id}")
     @ApiOperation(value = "根据ID删除",response = Response.class)
     public Response delete(@ApiParam(name = "id", value = "[字典项ID]")
                            @PathVariable("id") String id){
         this.service.delDictItem(id);
+        dictHelper.reload();
         return success();
     }
 
@@ -39,6 +44,7 @@ public class DictItemCtrl extends PureBaseCtrl<DictItemService> {
     public Response edit(@ApiParam(name = "dictItemEntity", value = "字典项对象")
            @RequestBody @Valid DictItemEntity dictItemEntity){
         this.service.editDictItem(dictItemEntity);
+        dictHelper.reload();
         return success(dictItemEntity);
     }
 }
