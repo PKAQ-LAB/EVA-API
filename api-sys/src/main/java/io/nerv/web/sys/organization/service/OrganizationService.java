70c692ddfa6fde4f389784b02149a4bf0ab6a4eb
Adameve <1135656594@qq.com>
2019-05-20 16:17:04 +0800

    组织管理中给新增的对象设置默认的orders字段

--- api-sys/src/main/java/io/nerv/web/sys/organization/service/OrganizationService.java
+++ api-sys/src/main/java/io/nerv/web/sys/organization/service/OrganizationService.java
@@ -98,7 +98,7 @@ public class OrganizationService extends BaseService<OrganizationMapper, Organiz
         }
         //如果是新增且orders属性为空则设置orders属性
         if (StrUtil.isBlank(organization.getId()) ) {
-            organization.setOrders(this.mapper.countPrantLeaf(pid););
+            organization.setOrders(this.mapper.countPrantLeaf(pid));
         }
         this.merge(organization);
         // 保存完重新查询一遍列表数据
