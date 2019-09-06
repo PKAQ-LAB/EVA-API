2c44f3d78f52cd3704fb9bd6655db7c8c6b24398
PKAQ <pkaq@msn.com>
2019-09-06 08:56:13 +0800

    鉴权模式可配置

--- eva-sys/src/main/java/io/nerv/security/entrypoint/UrlAccessDeniedHandler.java
+++ eva-sys/src/main/java/io/nerv/security/entrypoint/UrlAccessDeniedHandler.java
@@ -1,15 +1,18 @@
 package io.nerv.security.entrypoint;
 
 import com.alibaba.fastjson.JSON;
+import io.nerv.core.enums.ErrorCodeEnum;
 import io.nerv.core.mvc.util.Response;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.parameters.P;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * 自定义权限不足处理器
@@ -23,7 +26,12 @@ public class UrlAccessDeniedHandler implements AccessDeniedHandler {
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setStatus(HttpServletResponse.SC_FORBIDDEN);
 
-        response.getWriter().write(JSON.toJSONString(new Response().failure(401, "用户权限不足")));
+        try(PrintWriter printWriter = response.getWriter()){
+            printWriter.write(JSON.toJSONString(
+                                new Response()
+                                        .failure(401, "["+ ErrorCodeEnum.PERMISSION_DENY.getIndex() +"] 用户权限不足")));
+            printWriter.flush();
+        }
     }
 
 }
--- eva-sys/src/main/java/io/nerv/security/provider/UrlAccessDecisionManager.java
+++ eva-sys/src/main/java/io/nerv/security/provider/UrlAccessDecisionManager.java
@@ -3,6 +3,7 @@ package io.nerv.security.provider;
 import io.nerv.core.enums.ErrorCodeEnum;
 import io.nerv.security.exception.OathException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.AccessDecisionManager;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.ConfigAttribute;
@@ -17,20 +18,27 @@ import java.util.Collection;
 @Slf4j
 @Component
 public class UrlAccessDecisionManager implements AccessDecisionManager {
+    // 鉴权模式
+    @Value("${eva.security.mode}")
+    private String mode;
 
     // configAttributes 为MyInvocationSecurityMetadataSource的getAttributes(Object object)这个方法返回的结果，
     // 此方法是为了判定用户请求的url 是否在权限表中，如果在权限表中，则返回给 decide 方法，
     // 用来判定用户是否有此权限。
     @Override
     public void decide(Authentication authentication, Object o,
-                       Collection<ConfigAttribute> collection) throws AccessDeniedException, InsufficientAuthenticationException {
+                       Collection<ConfigAttribute> collection) throws AccessDeniedException {
         log.debug("collection=" + collection);
         for (ConfigAttribute configAttribute : collection) {
             // 当前请求需要的权限
             String needRole = configAttribute.getAttribute();
             log.debug("needRole=" + needRole);
-            if ("ROLE_USER".equals(needRole)) {
-                if (authentication instanceof AnonymousAuthenticationToken) {
+            if ("ROLE_USER".equals(needRole) && authentication instanceof AnonymousAuthenticationToken) {
+                // 所有未授权的菜单都会被认定为 需要 ROLE_USER 权限，
+                // 采用严格模式，只允许授权的url进行访问 所有未授权的不许访问
+                // 也可采用简单模式， 只对授权的url进行鉴权，未进行过授权配置的url都可访问
+                // 若采用第二种方式，可以通过用户登录后默认授予ROLE_USER权限 或 放开下面的注释即可
+                if (authentication instanceof AnonymousAuthenticationToken || "strict".equals(this.mode)) {
                     throw new OathException(ErrorCodeEnum.PERMISSION_DENY);
                 } else {
                     return;
@@ -45,7 +53,7 @@ public class UrlAccessDecisionManager implements AccessDecisionManager {
                 }
             }
         }
-        throw new AccessDeniedException("SimpleGrantedAuthority!!");
+        throw new AccessDeniedException(ErrorCodeEnum.PERMISSION_DENY.getName());
     }
 
     @Override
