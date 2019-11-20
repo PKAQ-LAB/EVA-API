459b172465e1e60b80bc3a115c75089e1cabdbd7
PKAQ <pkaq@msn.com>
2019-11-20 14:48:37 +0800

    防止返回字符串多出双引号

--- eva-sys/src/main/java/io/nerv/config/FastjsonConfiguration.java
+++ eva-sys/src/main/java/io/nerv/config/FastjsonConfiguration.java
@@ -13,9 +13,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.MediaType;
 
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -29,11 +30,17 @@ public class FastjsonConfiguration {
     @Autowired
     DictHelperProvider dictHelper;
 
-
     @Bean
     public HttpMessageConverters fastJsonHttpMessageConverters() {
         //定义一个转换消息的对象
         FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+
+        // 防止返回字符串多出双引号
+        List<MediaType> mediaTypes = new ArrayList<>(2);
+        mediaTypes.add(MediaType.valueOf(MediaType.TEXT_HTML_VALUE + ";charset=UTF-8"));
+        mediaTypes.add(MediaType.valueOf(MediaType.APPLICATION_JSON_VALUE + ";charset=UTF-8"));
+
+        fastConverter.setSupportedMediaTypes(mediaTypes);
 
         //添加fastjson的配置信息 比如 ：是否要格式化返回的json数据
         FastJsonConfig fastJsonConfig = new FastJsonConfig();
@@ -60,6 +67,6 @@ public class FastjsonConfiguration {
         //在转换器中添加配置信息
         fastConverter.setFastJsonConfig(fastJsonConfig);
 
-        return new HttpMessageConverters((HttpMessageConverter<?>) fastConverter);
+        return new HttpMessageConverters(fastConverter);
     }
 }
