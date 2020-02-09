bf27294ecc22a1cbe7ba3dc7e3d0f7a8429326f4
PKAQ <pkaq@msn.com>
2020-02-10 00:05:46 +0800

    license生成和验证

---
+++ nerv-license-server/src/main/java/io/nerv/license/LicenseManagerHolder.java
@@ -0,0 +1,26 @@
+package io.nerv.license;
+
+import de.schlichtherle.license.LicenseManager;
+import de.schlichtherle.license.LicenseParam;
+
+/**
+ * 证书管理器
+ */
+public class LicenseManagerHolder {
+
+    private static volatile LicenseManager licenseManager = null;
+
+    private LicenseManagerHolder() {
+    }
+
+    public static LicenseManager getLicenseManager(LicenseParam param) {
+        if (licenseManager == null) {
+            synchronized (LicenseManagerHolder.class) {
+                if (licenseManager == null) {
+                    licenseManager = new LicenseManager(param);
+                }
+            }
+        }
+        return licenseManager;
+    }
+}
