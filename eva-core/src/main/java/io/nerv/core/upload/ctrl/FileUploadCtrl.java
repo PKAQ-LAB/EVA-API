1e1f703372a26a401f7c12715f672a04265c6d10
PKAQ <pkaq@msn.com>
2020-02-20 14:28:57 +0800

    注释完善

--- eva-core/src/main/java/io/nerv/core/upload/ctrl/FileUploadCtrl.java
+++ eva-core/src/main/java/io/nerv/core/upload/ctrl/FileUploadCtrl.java
@@ -1,15 +1,10 @@
 package io.nerv.core.upload.ctrl;
 
-import cn.hutool.core.lang.Snowflake;
 import cn.hutool.core.map.MapUtil;
-import cn.hutool.core.util.IdUtil;
-import cn.hutool.core.util.StrUtil;
-import com.baomidou.mybatisplus.core.toolkit.StringUtils;
-import io.nerv.core.enums.BizCodeEnum;
-import io.nerv.core.exception.BizException;
 import io.nerv.core.mvc.util.Response;
 import io.nerv.core.upload.util.FileUploadProvider;
-import io.nerv.properties.EvaConfig;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
 
 /**
@@ -27,18 +21,16 @@ import java.io.IOException;
 @Slf4j
 @RestController
 @RequestMapping("/upload")
+@Api("上传")
 public class FileUploadCtrl {
-
-    @Autowired
-    private EvaConfig evaConfig;
 
     @Autowired
     private FileUploadProvider fileUploadProvider;
 
-    private Snowflake snowflake = IdUtil.createSnowflake(1, 1);
 
     @PostMapping("/file")
-    public Response uploadImage(MultipartFile file) throws IOException {
+    @ApiOperation(value = "文件上传",response = Response.class)
+    public Response upload(MultipartFile file) {
         String filePath = fileUploadProvider.upload(file);
 
         Response response = new Response();
