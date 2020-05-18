185385a72d6cdbe582cf85557e636bd717b257d0
PKAQ <pkaq@msn.com>
2020-05-18 09:14:31 +0800

    upgrade to 2.3.0
    (cherry picked from commit 8e9aae3cfae2943ee731dcc39968239c54b6a9dd)
    
    # Conflicts:
    #	build.gradle
    #	eva-web/src/main/java/io/nerv/WebBooter.java

--- nerv-admin-server/src/main/java/io/nerv/AdminBooter.java
+++ nerv-admin-server/src/main/java/io/nerv/AdminBooter.java
@@ -1,14 +1,10 @@
 package io.nerv;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
-import io.undertow.UndertowOptions;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
 
 /**
  * 启动类
@@ -19,10 +15,6 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class AdminBooter implements CommandLineRunner {
 
-
-    @Autowired
-    private GracefulShutdownUndertowWrapper gracefulShutdownUndertowWrapper;
-
     @Override
     public void run(String... args) {
         System.out.println("-------------------------- || EVA BOOT ADMIN STARTED || --------------------------");
@@ -31,16 +23,5 @@ public class AdminBooter implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(AdminBooter.class, args);
-    }
-
-    /**
-     * 用于接受 shutdown 事件
-     */
-    @Bean
-    public UndertowServletWebServerFactory servletWebServerFactory() {
-        UndertowServletWebServerFactory factory = new UndertowServletWebServerFactory();
-        factory.addDeploymentInfoCustomizers(deploymentInfo -> deploymentInfo.addOuterHandlerChainWrapper(gracefulShutdownUndertowWrapper));
-        factory.addBuilderCustomizers(builder -> builder.setServerOption(UndertowOptions.ENABLE_STATISTICS, true));
-        return factory;
     }
 }
