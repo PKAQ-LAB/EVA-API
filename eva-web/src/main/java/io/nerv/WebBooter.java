5f32334ad3404263080fe8d1e4be08a4a0684e51
PKAQ <pkaq@msn.com>
2020-08-21 21:14:27 +0800

    add nacos

--- eva-web/src/main/java/io/nerv/WebBooter.java
+++ eva-web/src/main/java/io/nerv/WebBooter.java
@@ -9,6 +9,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
@@ -20,6 +21,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @Slf4j
 @EnableCaching
 @EnableJpaAuditing
+@EnableDiscoveryClient
 @SpringBootApplication
 @ComponentScan(basePackages = {"io.nerv.*"})
 public class WebBooter implements CommandLineRunner {
