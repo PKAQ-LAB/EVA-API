5b54631c124a84d1e11af446b9cb9414ea90852c
PKAQ <pkaq@msn.com>
2020-08-25 09:27:43 +0800

    添加网关

---
+++ eva-cloud-gateway/src/main/java/io/nerv/GatewayBooter.java
@@ -0,0 +1,25 @@
+package io.nerv;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * 启动类
+ * @author PKAQ
+ */
+@Slf4j
+@SpringBootApplication
+public class GatewayBooter implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) {
+        log.info(" ------ eva cloud gateway  started ------ ");
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayBooter.class, args);
+    }
+}
