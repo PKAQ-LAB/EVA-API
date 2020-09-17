3f1855c9fdc5f814e334379b03718d7159b2046a
PKAQ <pkaq@msn.com>
2020-09-17 14:55:40 +0800

    v3

--- eva-cloud-gateway/src/main/java/io/nerv/config/ResourceServerConfig.java
+++ eva-cloud-gateway/src/main/java/io/nerv/config/ResourceServerConfig.java
@@ -1,104 +1,29 @@
 package io.nerv.config;
 
-import io.nerv.handler.ResAccessDeniedHandler;
-import io.nerv.handler.ResAuthenticationEntryPoint;
-import io.nerv.handler.ResAuthenticationFailureHandler;
-import io.nerv.handler.ResAuthenticationSuccessHandler;
-import io.nerv.manager.AuthorizationManager;
-import io.nerv.manager.TokenAuthenticationConverter;
-import io.nerv.manager.TokenAuthenticationManager;
+import io.nerv.manager.PermissionAuthorizationManager;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.security.authentication.ReactiveAuthenticationManager;
-import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.common.OAuth2AccessToken;
-import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
-import org.springframework.web.server.WebFilter;
 
 /**
  * 资源服务器配置
  */
-@Configuration
+@EnableWebFluxSecurity
 public class ResourceServerConfig {
     @Autowired
-    private AuthConfig authConfig;
-
-    @Autowired
-    private TokenStore tokenStore;
-
-    @Autowired
-    private AuthorizationManager authorizationManager;
+    private PermissionAuthorizationManager permissionAuthorizationManager;
 
     @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
 
-        // 认证处理器
-        ReactiveAuthenticationManager tokenAuthenticationManager = new TokenAuthenticationManager(tokenStore);
-        ResAuthenticationEntryPoint resAuthenticationEntryPoint = new ResAuthenticationEntryPoint();
-        // 拒绝访问处理
-        ResAccessDeniedHandler resAccessDeniedHandler = new ResAccessDeniedHandler();
+        http.authorizeExchange()
+                .pathMatchers("/security/**").permitAll()
+                .anyExchange().access(permissionAuthorizationManager);
 
-        //构建Bearer Token
-        //请求参数强制加上 Authorization BEARER token
-        serverHttpSecurity.addFilterAt((WebFilter) (exchange, chain) -> {
-            ServerHttpRequest request = exchange.getRequest();
-            String access_tk = request.getQueryParams().getFirst("access_token");
-
-            if( access_tk != null) {
-                exchange.getRequest().mutate().headers(httpHeaders ->
-                        httpHeaders.add(
-                                "Authorization",
-                                OAuth2AccessToken.BEARER_TYPE+" "+access_tk)
-                );
-            }
-            return chain.filter(exchange);
-        }, SecurityWebFiltersOrder.FIRST);
-
-        //身份认证
-        AuthenticationWebFilter authenticationWebFilter = new AuthenticationWebFilter(tokenAuthenticationManager);
-        //登陆验证失败
-        authenticationWebFilter.setAuthenticationFailureHandler(new ResAuthenticationFailureHandler());
-        //认证成功
-        authenticationWebFilter.setAuthenticationSuccessHandler(new ResAuthenticationSuccessHandler());
-
-        //token转换器
-        TokenAuthenticationConverter tokenAuthenticationConverter = new TokenAuthenticationConverter();
-        tokenAuthenticationConverter.setAllowUriQueryParameter(true);
-        authenticationWebFilter.setServerAuthenticationConverter(tokenAuthenticationConverter);
-
-        serverHttpSecurity.addFilterAt(authenticationWebFilter, SecurityWebFiltersOrder.AUTHENTICATION);
-
-        ServerHttpSecurity.AuthorizeExchangeSpec authorizeExchange = serverHttpSecurity.authorizeExchange();
-
-        //无需进行权限过滤的请求路径
-        authorizeExchange.matchers(EndpointRequest.toAnyEndpoint()).permitAll();
-        //无需进行权限过滤的请求路径
-        authorizeExchange.pathMatchers(authConfig.getAnonymous()).permitAll();
-
-        //option 请求默认放行
-        authorizeExchange
-                .pathMatchers(HttpMethod.OPTIONS).permitAll()
-                // 应用api权限控制
-                .anyExchange().access(authorizationManager)
-                //token 有效性控制
-                .and()
-                .exceptionHandling()
-                .accessDeniedHandler(resAccessDeniedHandler)
-                .authenticationEntryPoint(resAuthenticationEntryPoint)
-                .and()
-                .headers()
-                .frameOptions()
-                .disable()
-                .and()
-                .httpBasic().disable()
-                .csrf().disable();
-        return serverHttpSecurity.build();
+        http.oauth2ResourceServer().jwt();
+        http.csrf().disable();
+        return http.build();
     }
 }
---
+++ eva-cloud-gateway/src/main/java/io/nerv/manager/PermissionAuthorizationManager.java
@@ -0,0 +1,36 @@
+package io.nerv.manager;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.RandomUtils;
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.authorization.ReactiveAuthorizationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.web.server.authorization.AuthorizationContext;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class PermissionAuthorizationManager implements ReactiveAuthorizationManager<AuthorizationContext> {
+
+    /**
+     * 实现权限验证判断
+     */
+    @Override
+    public Mono<AuthorizationDecision> check(Mono<Authentication> authenticationMono, AuthorizationContext authorizationContext) {
+        ServerWebExchange exchange = authorizationContext.getExchange();
+        //请求资源
+        String requestPath = exchange.getRequest().getURI().getPath();
+        return authenticationMono.map(auth -> new AuthorizationDecision(checkAuthorities(exchange, auth, requestPath)))
+                                 .defaultIfEmpty(new AuthorizationDecision(false));
+    }
+    //权限校验
+    private boolean checkAuthorities(ServerWebExchange exchange, Authentication auth, String requestPath) {
+        Jwt principal = (Jwt) auth.getPrincipal();
+        log.info("访问的URL是：{}用户信息:{}",requestPath, principal.getClaims().get("user_name"));
+        return RandomUtils.nextInt() % 2 == 0 ;
+    }
+
+}
