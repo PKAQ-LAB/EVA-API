0f00e9cd6029af8aa6a0066aa0f93a014f14e868
PKAQ <pkaq@msn.com>
2020-10-13 22:55:41 +0800

    upgrade

--- eva-web/src/main/java/io/nerv/WebBooter.java
+++ eva-web/src/main/java/io/nerv/WebBooter.java
@@ -1,7 +1,5 @@
 package io.nerv;
 
-import io.nerv.core.license.LicenseVerify;
-import io.nerv.properties.EvaConfig;
 import io.nerv.web.sys.dict.cache.DictCacheHelper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
+
 
 /**
  * 启动类
@@ -22,12 +21,6 @@ import org.springframework.context.annotation.ComponentScan;
 public class WebBooter implements CommandLineRunner {
 
     @Autowired
-    private EvaConfig evaConfig;
-
-    @Autowired(required = false)
-    private LicenseVerify licenseVerify;
-
-    @Autowired
     private DictCacheHelper dictCacheHelper;
 
     @Override
@@ -36,16 +29,6 @@ public class WebBooter implements CommandLineRunner {
         this.dictCacheHelper.init();
         this.dictCacheHelper.getAll();
         log.info(" ---- 字典初始化 结束 ---- ");
-        if (evaConfig.getLicense().isEnable()){
-            // 安装license
-            licenseVerify.init();
-
-            // 验证license
-            if (!licenseVerify.vertify()) {
-                log.error("授权验证未通过, 请更新授权文件");
-                Runtime.getRuntime().halt(1);
-            }
-        }
     }
 
     public static void main(String[] args) {
