8c8e2f08a941057dc546e276c4762a1eab993c81
PKAQ <pkaq@msn.com>
2021-03-29 21:36:54 +0800

    支持oracle

--- eva-web/eva-web-sys/src/main/java/io/nerv/log/error/ctrl/ErrorCtrl.java
+++ eva-web/eva-web-sys/src/main/java/io/nerv/log/error/ctrl/ErrorCtrl.java
@@ -1,6 +1,8 @@
 package io.nerv.log.error.ctrl;
 
+import cn.hutool.core.date.DateUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import io.nerv.core.bizlog.supporter.mybatis.mapper.MybatisSupporterMapper;
 import io.nerv.core.exception.entity.ErrorlogEntity;
 import io.nerv.core.exception.mapper.ErrorlogMapper;
 import io.nerv.core.mvc.util.Page;
@@ -9,21 +11,31 @@ import io.nerv.core.mvc.vo.Response;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.mapping.DatabaseIdProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
+import java.sql.SQLException;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 @RestController
 @RequestMapping("/monitor/log/error")
 @Api(tags = "错误日志")
+@RequiredArgsConstructor
 public class ErrorCtrl {
 
-    @Autowired
-    private ErrorlogMapper errorlogMapper;
+    private final ErrorlogMapper errorlogMapper;
+
+    private final DatabaseIdProvider databaseIdProvider;
+
+    private final DataSource dataSource;
 
     @GetMapping({"/get/{id}"})
     @ApiOperation(value = "根据id获取操作日志明细",response = Response.class)
@@ -36,22 +48,29 @@ public class ErrorCtrl {
     @ApiOperation(value = "获取错误日志列表", response = Response.class)
     public Response list(@ApiParam(name ="dateRange", value = "查询区间") DateRangeVo dateRange,
                          @ApiParam(name ="pageNo", value = "页码") Integer pageNo,
-                         @ApiParam(name ="pageCount", value = "条数") Integer pageCount){
+                         @ApiParam(name ="pageCount", value = "条数") Integer pageCount) throws SQLException {
 
         QueryWrapper<ErrorlogEntity> wrapper = new QueryWrapper<>();
 
-        LocalDate begin = dateRange.getBegin();
-        LocalDate end = LocalDate.now();
+        Date begin = dateRange.getBegin();
+        Date end = dateRange.getEnd();
 
         if (null == begin){
-            begin = LocalDate.now().minusDays(7);
+            begin = DateUtil.offsetDay(new Date(), -7);
         }
         if (null == end){
-            end = LocalDate.now();
+            end = new Date();
         }
 
-        wrapper.ge("REQUEST_TIME", begin);
-        wrapper.le("REQUEST_TIME", end);
+        String dbType = databaseIdProvider.getDatabaseId(dataSource);
+
+        if ("oracle".equalsIgnoreCase(dbType)){
+            wrapper.ge("REQUEST_TIME", DateUtil.format(begin, DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+            wrapper.le("REQUEST_TIME", DateUtil.format(end, DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+        } else if ("mysql".equalsIgnoreCase(dbType)) {
+            wrapper.ge("REQUEST_TIME", begin);
+            wrapper.le("REQUEST_TIME", end);
+        }
 
         Page pagination = new Page();
         pagination.setCurrent(pageNo == null? 1 : pageNo);
