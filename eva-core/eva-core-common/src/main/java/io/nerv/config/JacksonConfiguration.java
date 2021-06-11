64a4d74764ceff03ffdb55032412bf6430e9b1c5
PKAQ <pkaq@msn.com>
2021-06-11 09:38:12 +0800

    json忽略不存在属性

--- eva-core/eva-core-common/src/main/java/io/nerv/config/JacksonConfiguration.java
+++ eva-core/eva-core-common/src/main/java/io/nerv/config/JacksonConfiguration.java
@@ -1,6 +1,7 @@
 package io.nerv.config;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,6 +20,8 @@ public class JacksonConfiguration {
         ObjectMapper mapper = new ObjectMapper();
                      mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
                      mapper.setSerializationInclusion(JsonInclude.Include.ALWAYS);
+                     // 忽略类中未知属性
+                     mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,false);
         converter.setObjectMapper(mapper);
         return converter;
     }
