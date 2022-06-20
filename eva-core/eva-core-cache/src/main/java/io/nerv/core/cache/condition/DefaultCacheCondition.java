8dea2edb0dc4a8caa59bd09a8c2ffa4be2d19f05
PKAQ <pkaq@msn.com>
2022-06-20 16:07:52 +0800

    .

--- eva-core/eva-core-cache/src/main/java/io/nerv/core/cache/condition/DefaultCacheCondition.java
+++ eva-core/eva-core-cache/src/main/java/io/nerv/core/cache/condition/DefaultCacheCondition.java
@@ -1,18 +1,28 @@
 package io.nerv.core.cache.condition;
 
+import io.nerv.properties.Cache;
+import io.nerv.properties.EvaConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.core.type.AnnotatedTypeMetadata;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 /**
  * 缓存启用条件， 默认使用 caffeine 缓存
  * @author: S.PKAQ
  */
+@Component
 public class DefaultCacheCondition implements Condition {
 
+    @Autowired
+    private EvaConfig evaconfig;
     @Override
     public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-        String cacheType = context.getEnvironment().getProperty("eva.cache.type");
+        System.out.println(evaconfig.getCache().getType()+"------------------------->");
+        String cacheType =  Optional.ofNullable(evaconfig.getCache()).map(Cache::getType).orElse("redis");
         return null == cacheType || "".equals(cacheType) || "caffeine".equals(cacheType);
     }
 }
