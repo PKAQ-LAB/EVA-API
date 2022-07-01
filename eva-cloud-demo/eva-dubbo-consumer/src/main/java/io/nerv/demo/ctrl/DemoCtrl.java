028657a0416d68887f991c790e1dd2d56b46ec3a
PKAQ <pkaq@msn.com>
2022-07-01 08:47:18 +0800

    sentinel support

--- eva-cloud-demo/eva-dubbo-consumer/src/main/java/io/nerv/demo/ctrl/DemoCtrl.java
+++ eva-cloud-demo/eva-dubbo-consumer/src/main/java/io/nerv/demo/ctrl/DemoCtrl.java
@@ -1,5 +1,6 @@
 package io.nerv.demo.ctrl;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import io.nerv.dubbo.DemoService;
 import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,5 +17,17 @@ public class DemoCtrl {
     public String call(){
        var s =  demoService.print();
        return s;
+    }
+
+    @GetMapping("/ex")
+    @SentinelResource(value = "fallback", fallback = "handleFallback")
+    public String ex() throws Exception {
+        if (true){
+            throw new Exception("exception");
+        }
+        return "ex method";
+    }
+    public String handleFallback(){
+        return "sentinel fallback called";
     }
 }
