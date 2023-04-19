9948e5032f315864ae8600fccc02fe0fa7b58d2a
PKAQ <pkaq@msn.com>
2023-04-19 17:44:55 +0800

    base vo

---
+++ eva-core/eva-core-common/src/main/java/io/nerv/core/mvc/vo/StdVo.java
@@ -0,0 +1,10 @@
+package io.nerv.core.mvc.vo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class StdVo implements Serializable {
+    private String id;
+}
---
+++ eva-core/eva-core-common/src/main/java/io/nerv/core/mvc/vo/Vo.java
@@ -0,0 +1,6 @@
+package io.nerv.core.mvc.vo;
+
+import java.io.Serializable;
+
+public interface Vo extends Serializable {
+}
