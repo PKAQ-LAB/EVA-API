21c4e286b79a93c87d04578c821e48553c10c69e
Adameve <1135656594@qq.com>
2023-05-22 15:40:59 +0800

    请求处理工具类获取请求ip

--- yunyue-core/yunyue-core-web/src/main/java/tech/yunyue/core/web/util/RequestUtil.java
+++ yunyue-core/yunyue-core-web/src/main/java/tech/yunyue/core/web/util/RequestUtil.java
@@ -1,6 +1,7 @@
 package tech.yunyue.core.web.util;
 
 import cn.hutool.core.util.StrUtil;
+import lombok.extern.slf4j.Slf4j;
 import tech.yunyue.core.constant.CommonConstant;
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -8,6 +9,7 @@ import jakarta.servlet.http.HttpServletRequest;
 /**
  * 请求处理工具类
  */
+@Slf4j
 public class RequestUtil {
     /**
      * 获取请求设备类型
@@ -42,5 +44,45 @@ public class RequestUtil {
      */
     public static String formatDeivceAndVersion(HttpServletRequest request, String format) {
         return String.format(format, getDeivce(request), getVersion(request));
+    }
+
+    /**
+     * 获取请求ip
+     * @param request
+     * @return
+     */
+    public static String getIpAddr(HttpServletRequest request) {
+        String ip = null;
+        try {
+             ip = request.getHeader("X-Real-IP");
+            if (StrUtil.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("x-forwarded-for");
+            }
+            if (StrUtil.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("Proxy-Client-IP");
+            }
+            if (StrUtil.isEmpty(ip) || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("WL-Proxy-Client-IP");
+            }
+            if (StrUtil.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("HTTP_CLIENT_IP");
+            }
+            if (StrUtil.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+            }
+            if (StrUtil.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getRemoteAddr();
+            }
+        } catch (Exception e) {
+            log.error("IPUtils ERROR：",e);
+        }
+        //对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
+        if (!StrUtil.isEmpty(ip) && ip.length() > 15) {
+            var index = ip.indexOf(",");
+            if (index > 0) {
+                ip = ip.substring(0, index);
+            }
+        }
+        return ip;
     }
 }
