a4c66f696e7194bf067dc471aa2b51ce05164c9e
Adameve <1135656594@qq.com>
2023-05-31 15:19:02 +0800

    黑名单管理service加上操作日志

--- yunyue-web/yunyue-web-sys/src/main/java/tech/yunyue/sys/blacklist/service/BlackListService.java
+++ yunyue-web/yunyue-web-sys/src/main/java/tech/yunyue/sys/blacklist/service/BlackListService.java
@@ -1,9 +1,12 @@
 package tech.yunyue.sys.blacklist.service;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
+import tech.yunyue.core.log.annotation.BizLog;
+import tech.yunyue.core.log.base.BizLogEnum;
 import tech.yunyue.core.mybatis.mvc.service.mybatis.StdService;
 import tech.yunyue.sys.blacklist.cache.BlackListCacheHelper;
 import tech.yunyue.sys.blacklist.entity.BlackListEntity;
@@ -17,6 +20,7 @@ import java.util.stream.Collectors;
  */
 @Service
 @RequiredArgsConstructor
+@Schema(description = "黑名单管理")
 public class BlackListService extends StdService<BlackListMapper, BlackListEntity> {
     private final BlackListCacheHelper blackListCacheHelper;
 
@@ -29,6 +33,7 @@ public class BlackListService extends StdService<BlackListMapper, BlackListEntit
         blackListCacheHelper.cachePut(blackList);
     }
 
+    @BizLog(operateType= BizLogEnum.CREATE_UPDATE,description = "保存黑名单列表")
     @Override
     public void merge(BlackListEntity entity) {
         //新增/修改缓存
@@ -42,6 +47,7 @@ public class BlackListService extends StdService<BlackListMapper, BlackListEntit
         super.merge(entity);
     }
 
+    @BizLog(operateType= BizLogEnum.DELETE,description = "删除黑名单列表")
     @Override
     public void delete(ArrayList<String> param) {
         super.delete(param);
