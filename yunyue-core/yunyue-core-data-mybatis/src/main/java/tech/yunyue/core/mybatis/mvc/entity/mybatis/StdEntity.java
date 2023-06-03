cdd8ed906466cfaa382644b8008e4c903160364b
Adameve <1135656594@qq.com>
2023-06-03 16:05:54 +0800

    反序列化时处理LocalDateTime

--- yunyue-core/yunyue-core-data-mybatis/src/main/java/tech/yunyue/core/mybatis/mvc/entity/mybatis/StdEntity.java
+++ yunyue-core/yunyue-core-data-mybatis/src/main/java/tech/yunyue/core/mybatis/mvc/entity/mybatis/StdEntity.java
@@ -2,7 +2,9 @@ package tech.yunyue.core.mybatis.mvc.entity.mybatis;
 
 import com.baomidou.mybatisplus.annotation.*;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import tech.yunyue.core.mvc.entity.Entity;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -32,6 +34,7 @@ public abstract class StdEntity implements Entity {
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime gmtCreate;
 
     @Schema(description = "修改人")
@@ -43,6 +46,7 @@ public abstract class StdEntity implements Entity {
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime gmtModify;
 
     /** 逻辑删除 */
