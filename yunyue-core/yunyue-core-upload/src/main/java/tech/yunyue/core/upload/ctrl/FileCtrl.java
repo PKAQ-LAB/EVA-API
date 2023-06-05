6966e2f50943fa7b9690e3b276f1f752fc424589
Adameve <1135656594@qq.com>
2023-06-05 17:44:15 +0800

    提供文件下载接口

--- yunyue-core/yunyue-core-upload/src/main/java/tech/yunyue/core/upload/ctrl/FileCtrl.java
+++ yunyue-core/yunyue-core-upload/src/main/java/tech/yunyue/core/upload/ctrl/FileCtrl.java
@@ -1,6 +1,9 @@
 package tech.yunyue.core.upload.ctrl;
 
 import cn.hutool.core.map.MapUtil;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import tech.yunyue.core.mvc.vo.Response;
 import tech.yunyue.core.upload.util.FileProvider;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +12,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.net.URLEncoder;
 
 /**
  * 文件Ctrl
@@ -32,5 +38,21 @@ public class FileCtrl {
     public Response upload(MultipartFile file, String path) {
         String filePath = fileProvider.upload(file, path);
         return new Response().success(MapUtil.of("pname", filePath));
+    }
+
+    /**
+     * 文件下载
+     */
+    @GetMapping("/download")
+    public void download(HttpServletResponse response, String fileName) throws IOException {
+        response.setCharacterEncoding("utf-8");
+        response.setContentType(MediaType.APPLICATION_OCTET_STREAM_VALUE);
+        var name = fileName.substring(fileName.lastIndexOf("/")+1);;
+        //非图片文件返回原文件名
+        if(!FileProvider.isPicture(fileName)){
+            name = name.substring(name.lastIndexOf(":")+1);
+        }
+        response.addHeader("Content-Disposition", "attachment;fileName=" + URLEncoder.encode(name, "UTF-8"));
+        fileProvider.downLoad(fileName, response.getOutputStream());
     }
 }
