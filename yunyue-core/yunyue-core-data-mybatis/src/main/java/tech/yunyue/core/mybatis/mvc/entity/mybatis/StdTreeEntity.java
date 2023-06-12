3039fd8706cefd7489594c629e9ac6debcd5d78f
Adameve <1135656594@qq.com>
2023-06-12 17:21:38 +0800

    orders字段默认为null

--- yunyue-core/yunyue-core-data-mybatis/src/main/java/tech/yunyue/core/mybatis/mvc/entity/mybatis/StdTreeEntity.java
+++ yunyue-core/yunyue-core-data-mybatis/src/main/java/tech/yunyue/core/mybatis/mvc/entity/mybatis/StdTreeEntity.java
@@ -34,7 +34,7 @@ public class StdTreeEntity extends StdEntity {
     private String parentName;
 
     @Schema(description = "排序")
-    private Integer orders;
+    private Long orders;
     @Schema(description = "路径")
     private String path;
 
