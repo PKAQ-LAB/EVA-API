3abc2b0ffdd1ce6b1598ca7947983827572a6096
Adameve <1135656594@qq.com>
2023-07-05 18:12:59 +0800

    组织类型-字典标识

--- yunyue-core/yunyue-core-common/src/main/java/tech/yunyue/core/constant/CommonConstant.java
+++ yunyue-core/yunyue-core-common/src/main/java/tech/yunyue/core/constant/CommonConstant.java
@@ -68,7 +68,7 @@ public interface CommonConstant {
     // 把全局资源code保存在redis中的前缀
     String REDIS_RESOURCE_CODE_PREFIX_KEY = "GLOBAL:RESOURCE:CODE:";
     // 组织类型-字典标识
-    String ORGANIZATION_TYPE_CODE = "organization_type";
+    String ORGANIZATION_TYPE_CODE = "dict_organization_type";
     // 把用户保存在redis中的前缀
     String REDIS_USER_INFO_PREFIX_KEY = "USER_INFO:";
 
