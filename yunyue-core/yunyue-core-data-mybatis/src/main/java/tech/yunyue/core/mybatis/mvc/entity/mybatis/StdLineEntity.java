9677a5a35a568e247b0727559d51b93c6a49f28f
Adameve <1135656594@qq.com>
2023-07-11 11:03:36 +0800

    子表实体类基类

---
+++ yunyue-core/yunyue-core-data-mybatis/src/main/java/tech/yunyue/core/mybatis/mvc/entity/mybatis/StdLineEntity.java
@@ -0,0 +1,25 @@
+package tech.yunyue.core.mybatis.mvc.entity.mybatis;
+
+import com.baomidou.mybatisplus.annotation.*;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import org.apache.ibatis.type.JdbcType;
+import tech.yunyue.core.mvc.entity.Entity;
+
+/**
+ * 子表实体类基类，用于存放公共属性
+ */
+@Data
+public abstract class StdLineEntity implements Entity {
+    @TableId(type = IdType.ASSIGN_UUID)
+    @TableField(jdbcType = JdbcType.VARCHAR)
+    private String id;
+
+    @Schema(description = "主表id")
+    private String mainId;
+
+    @Schema(description = "租户id")
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private String tenantId;
+}
