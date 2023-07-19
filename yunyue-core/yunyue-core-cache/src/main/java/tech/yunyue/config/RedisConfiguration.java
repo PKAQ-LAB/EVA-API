9cc06d3bea70d05436e8b5cb9ae2c39e87f79b9f
Adameve <1135656594@qq.com>
2023-07-19 20:01:17 +0800

    RedisTemplate<Object, Object>使用GenericJackson2JsonRedisSerializer实现序列化  并自定义时间类型转换

--- yunyue-core/yunyue-core-cache/src/main/java/tech/yunyue/config/RedisConfiguration.java
+++ yunyue-core/yunyue-core-cache/src/main/java/tech/yunyue/config/RedisConfiguration.java
@@ -1,5 +1,16 @@
 package tech.yunyue.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.redis.serializer.*;
 import tech.yunyue.core.cache.condition.RedisCacheCondition;
 import tech.yunyue.core.properties.EvaConfig;
 import lombok.RequiredArgsConstructor;
@@ -13,11 +24,13 @@ import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import java.lang.reflect.Field;
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,9 +41,17 @@ import java.util.Map;
 @Configuration
 @Conditional(RedisCacheCondition.class)
 @RequiredArgsConstructor
+@EnableCaching
 public class RedisConfiguration {
 
     private final EvaConfig evaConfig;
+
+    public static final String DATE_TIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
+    public static final String DATE_PATTERN = "yyyy-MM-dd";
+    public static final String TIME_PATTERN = "HH:mm:ss";
+    public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern(DATE_TIME_PATTERN);
+    public static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern(DATE_PATTERN);
+    public static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern(TIME_PATTERN);
 
     /**
      * @return 返回类型
@@ -44,10 +65,38 @@ public class RedisConfiguration {
         //key序列化
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         //value序列化
-        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<Object>(Object.class));
+        redisTemplate.setValueSerializer(getValueSerializer());
 
         redisTemplate.afterPropertiesSet();
         return redisTemplate;
+    }
+
+    private RedisSerializer getValueSerializer(){
+        // 指定相应的序列化方案
+        GenericJackson2JsonRedisSerializer valueSerializer = new GenericJackson2JsonRedisSerializer();
+        // 通过反射获取Mapper对象, 增加一些配置, 增强兼容性
+        try {
+            Field field = GenericJackson2JsonRedisSerializer.class.getDeclaredField("mapper");
+            field.setAccessible(true);
+            ObjectMapper objectMapper = (ObjectMapper) field.get(valueSerializer);
+            // 配置[忽略未知字段]
+            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            // 配置[时间类型转换]
+            JavaTimeModule timeModule = new JavaTimeModule();
+            // LocalDateTime序列化与反序列化
+            timeModule.addSerializer(new LocalDateTimeSerializer(DATE_TIME_FORMATTER));
+            timeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DATE_TIME_FORMATTER));
+            // LocalDate序列化与反序列化
+            timeModule.addSerializer(new LocalDateSerializer(DATE_FORMATTER));
+            timeModule.addDeserializer(LocalDate.class, new LocalDateDeserializer(DATE_FORMATTER));
+            // LocalTime序列化与反序列化
+            timeModule.addSerializer(new LocalTimeSerializer(TIME_FORMATTER));
+            timeModule.addDeserializer(LocalTime.class, new LocalTimeDeserializer(TIME_FORMATTER));
+            objectMapper.registerModule(timeModule);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+        return valueSerializer;
     }
 
     @Bean
