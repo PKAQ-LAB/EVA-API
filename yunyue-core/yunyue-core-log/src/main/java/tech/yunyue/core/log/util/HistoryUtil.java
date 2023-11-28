083caf278c651b488cf40f49e9a2e9c9307d29a7
Adameve <1135656594@qq.com>
2023-11-28 09:38:45 +0800

    将获取当前线程操作人姓名的方法放在外部

--- yunyue-core/yunyue-core-log/src/main/java/tech/yunyue/core/log/util/HistoryUtil.java
+++ yunyue-core/yunyue-core-log/src/main/java/tech/yunyue/core/log/util/HistoryUtil.java
@@ -20,7 +20,6 @@ import tech.yunyue.core.log.constant.LogConstant;
 import tech.yunyue.core.threaduser.ThreadUserHelper;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Component
 public class HistoryUtil<T> {
@@ -31,26 +30,28 @@ public class HistoryUtil<T> {
     public static final Sort sort = Sort.by(Sort.Direction.DESC, "_id");
 
     @PostConstruct
-    public void init(){
+    public void init() {
         mongoTemplate = template;
     }
 
     /**
      * 存储历史数据到mongo中 entity对象需要有@TableName或者@HistoryLog标识表名
+     *
      * @param entity
      */
     public void sava(T entity) {
         Class clazz = Optional.ofNullable(entity).orElseThrow(BizCodeEnum.HISTORY_LOG_CAN_NOT_NULL::newException).getClass();
         var tableName = getTableName(clazz);
         // 同类调用异步方法不生效 使用当前类的代理对象调用
-        getSelf().save(tableName, (HistoryLog) clazz.getAnnotation(HistoryLog.class), entity);
+        String createName = Optional.ofNullable(ThreadUserHelper.getUserName()).orElse(ThreadUserHelper.getAccount());
+        getSelf().save(tableName, (HistoryLog) clazz.getAnnotation(HistoryLog.class), entity, createName);
     }
 
     /**
      * 把数据快照异步插入mongo
      */
     @Async("log_task")
-    protected void save(String tableName, HistoryLog historyLog, T entity) {
+    protected void save(String tableName, HistoryLog historyLog, T entity, String name) {
         // 在mongo表中设置mongo_mark_id字段 做为该表的数据标识
         // 因为有的实体类没有id属性 可以用别的唯一属性作为标识 不管用什么做标识 在mongo中就映射到mongo_mark_id属性上
         JSONObject object = JSONUtil.parseObj(entity);
@@ -60,35 +61,37 @@ public class HistoryUtil<T> {
         }
         object.set(LogConstant.MONGO_HISTORY_TABLE_MARK, object.get(markName));
         object.set(LogConstant.MONGO_CREATE_TIME, DateUtil.now());
-        object.set(LogConstant.MONGO_CREATE_NAME, Optional.ofNullable(ThreadUserHelper.getUserName()).orElse(ThreadUserHelper.getAccount()));
+        object.set(LogConstant.MONGO_CREATE_NAME, name);
         object.set(LogConstant.MONGO_EXPIRE_TIME, new Date());
         mongoTemplate.save(object, tableName);
     }
 
     /**
      * 根据数据id返回该条数据所有的修改记录 key是mongo表中的id 比较数据时需要该id
-     * @param id  需要查询历史快照的数据的标识 在@HistoryLog注解中以什么字段为标识则传什么 默认为id
+     *
+     * @param id    需要查询历史快照的数据的标识 在@HistoryLog注解中以什么字段为标识则传什么 默认为id
      * @param clazz 需要转换的数据类型  有@TableName或者@HistoryLog标识表名
      * @return 有序map
      */
-    public static <T> List<JSONObject> getModifyRecords(String id, Class<T> clazz){
+    public static <T> List<JSONObject> getModifyRecords(String id, Class<T> clazz) {
         return getModifyRecords(getTableName(clazz), id);
     }
 
     /**
      * 根据集合名称和数据id返回该条数据所有的修改记录 key是mongo表中的id 比较数据时需要该id
-     * @param collectionName  集合名称
-     * @param id  需要查询历史快照的数据的标识 在@HistoryLog注解中以什么字段为标识则传什么 默认为id
-     * @param clazz 需要转换的数据类型
+     *
+     * @param collectionName 集合名称
+     * @param id             需要查询历史快照的数据的标识 在@HistoryLog注解中以什么字段为标识则传什么 默认为id
+     * @param clazz          需要转换的数据类型
      * @return 有序map
      */
-    public static List<JSONObject> getModifyRecords(String collectionName, String id){
+    public static List<JSONObject> getModifyRecords(String collectionName, String id) {
         var query = Query.query(Criteria.where(LogConstant.MONGO_HISTORY_TABLE_MARK).is(id)).with(sort);
-        query.fields().include(LogConstant.MONGO_HISTORY_TABLE_MARK,LogConstant.MONGO_CREATE_TIME,LogConstant.MONGO_CREATE_NAME);
+        query.fields().include(LogConstant.MONGO_HISTORY_TABLE_MARK, LogConstant.MONGO_CREATE_TIME, LogConstant.MONGO_CREATE_NAME);
         var list = mongoTemplate
                 .find(query, Map.class, collectionName);
-        return list.stream().map(e->{
-            e.put("mid",e.get("_id").toString());
+        return list.stream().map(e -> {
+            e.put("mid", e.get("_id").toString());
             e.remove("_id");
             return JSONUtil.toBean(JSONUtil.parseObj(e), JSONObject.class);
         }).toList();
@@ -96,19 +99,22 @@ public class HistoryUtil<T> {
 
     /**
      * 返回mid对应的修改记录和前一次修改记录  new对应本次  old对应前一次
-     * @param mid mongo中的id
+     *
+     * @param mid   mongo中的id
      * @param clazz 需要转换的数据类型 有@TableName或者@HistoryLog标识表名
      */
-    public static <T> Map<String,T> contrastLast(String mid, Class<T> clazz){
+    public static <T> Map<String, T> contrastLast(String mid, Class<T> clazz) {
         return contrastLast(getTableName(clazz), mid, clazz);
     }
+
     /**
      * 返回mid对应的修改记录和前一次修改记录  new对应本次  old对应前一次
+     *
      * @param collectionName 集合名称
-     * @param mid mongo中的id
-     * @param clazz 需要转换的数据类型
+     * @param mid            mongo中的id
+     * @param clazz          需要转换的数据类型
      */
-    public static <T> Map<String,T> contrastLast(String collectionName, String mid, Class<T> clazz){
+    public static <T> Map<String, T> contrastLast(String collectionName, String mid, Class<T> clazz) {
         Map<String, T> map = new HashMap<>(2);
         ObjectId objectId = new ObjectId(mid);
         // 根据mid查出本次的修改  不能直接转clazz
@@ -125,27 +131,29 @@ public class HistoryUtil<T> {
 
     /**
      * 根据mid返回对应的修改记录
-     * @param mid mongo中的id
+     *
+     * @param mid   mongo中的id
      * @param clazz 需要转换的数据类型 有@TableName或者@HistoryLog标识表名
      */
-    public static <T> T getById(String mid, Class<T> clazz){
+    public static <T> T getById(String mid, Class<T> clazz) {
         return getById(getTableName(clazz), mid, clazz);
     }
 
     /**
      * 根据mid返回对应的修改记录
+     *
      * @param collectionName 集合名称
-     * @param mid mongo中的id
-     * @param clazz 需要转换的数据类型
+     * @param mid            mongo中的id
+     * @param clazz          需要转换的数据类型
      */
-    public static <T> T getById(String collectionName, String mid, Class<T> clazz){
+    public static <T> T getById(String collectionName, String mid, Class<T> clazz) {
         // 根据mid查出本次的修改  不能直接转clazz
         var obj = mongoTemplate.findById(new ObjectId(mid), JSONObject.class, collectionName);
         return JSONUtil.toBean(obj, clazz);
     }
 
-    HistoryUtil getSelf(){
-        if (self == null){
+    HistoryUtil getSelf() {
+        if (self == null) {
             self = SpringUtil.getBean(HistoryUtil.class);
         }
         return self;
