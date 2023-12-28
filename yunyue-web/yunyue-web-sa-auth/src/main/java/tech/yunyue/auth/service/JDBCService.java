7c9c2134cb411d270cedd39847de8e1cd0c1dc77
Adameve <1135656594@qq.com>
2023-12-28 15:12:44 +0800

    用户根据账号@租户code登录

--- yunyue-web/yunyue-web-sa-auth/src/main/java/tech/yunyue/auth/service/JDBCService.java
+++ yunyue-web/yunyue-web-sa-auth/src/main/java/tech/yunyue/auth/service/JDBCService.java
@@ -23,36 +23,37 @@ public class JDBCService {
     private final JdbcTemplate jdbcTemplate;
 
     /**
-     * 根据账号、电话、email查询用户
+     * 根据账号@租户code、电话查询用户
      */
-    public Map<String, Object> loadUserByUsername(String account){
+    public Map<String, Object> loadUserByUsername(String account) {
         try {
             String sql = "SELECT ID,ACCOUNT,TEL,PASSWORD,LOCKED,DEPT_ID,DEPT_NAME,NAME,NICK_NAME,TENANT_ID,U_POST_ID,POST_NAME " +
                     "FROM SYS_USER_INFO SU " +
-                    "WHERE  DELETED = '0000' AND (SU.ACCOUNT = ? OR SU.TEL = ?)";
+                    "WHERE  DELETED = '0000' AND (IF(ISNULL(SU.TENANT_ID), SU.ACCOUNT, CONCAT(ACCOUNT,'@',TENANT_CODE)) = ? OR SU.TEL = ?)";
+
             return this.jdbcTemplate.queryForMap(sql, account, account);
-        }catch (EmptyResultDataAccessException e){
+        } catch (EmptyResultDataAccessException e) {
             BizCodeEnum.ACCOUNT_NOT_EXIST.newException();
         }
         return Collections.emptyMap();
     }
 
     // 因为用户登录时存的是jsonStirng 所以这边也存string
-    @Cacheable(cacheNames = CommonConstant.CACHE_USERDATA, key = "'"+ CommonConstant.REDIS_USER_INFO_PREFIX_KEY + "'" + "+#userId")
-    public String loadUserById(String userId){
+    @Cacheable(cacheNames = CommonConstant.CACHE_USERDATA, key = "'" + CommonConstant.REDIS_USER_INFO_PREFIX_KEY + "'" + "+#userId")
+    public String loadUserById(String userId) {
         return JsonUtil.toJson(JwtUserFactory.create(loadUserMapById(userId), Collections.emptyMap()));
     }
 
     /**
      * 根据id查询用户
      */
-    private Map<String, Object> loadUserMapById(String userId){
+    private Map<String, Object> loadUserMapById(String userId) {
         try {
             String sql = "SELECT ID,ACCOUNT,TEL,PASSWORD,LOCKED,DEPT_ID,DEPT_NAME,NAME,NICK_NAME,TENANT_ID,U_POST_ID,POST_NAME " +
                     "FROM SYS_USER_INFO SU " +
                     "WHERE  DELETED = '0000' AND ID = ?";
             return this.jdbcTemplate.queryForMap(sql, userId);
-        }catch (EmptyResultDataAccessException e){
+        } catch (EmptyResultDataAccessException e) {
             BizCodeEnum.ACCOUNT_NOT_EXIST.newException();
         }
         return Collections.emptyMap();
@@ -61,30 +62,32 @@ public class JDBCService {
     /**
      * 查询用户拥有的角色 并转成ThreadUser.GrantedRoles对象 且缓存在redis中
      */
-    @Cacheable(cacheNames = CommonConstant.CACHE_USERDATA, key = "'"+ CommonConstant.REDIS_USER_ROLES_PREFIX_KEY + "'" + "+#userId")
-    public Map<String, ThreadUser.GrantedRoles> getRoleById(String userId){
+    @Cacheable(cacheNames = CommonConstant.CACHE_USERDATA, key = "'" + CommonConstant.REDIS_USER_ROLES_PREFIX_KEY + "'" + "+#userId")
+    public Map<String, ThreadUser.GrantedRoles> getRoleById(String userId) {
         return JwtUserFactory.mapToGrantedAuthorities(getRoleMapById(userId));
     }
+
     /**
      * 查询用户拥有的角色 包括角色的数据权限类型
      */
-    private List<Map<String, Object>> getRoleMapById(String userId){
-        try{
+    private List<Map<String, Object>> getRoleMapById(String userId) {
+        try {
             String sql = "SELECT SR.ID, SR.NAME, SR.CODE, IFNULL(DATA_PERMISSION_TYPE, '0000') DATA_PERMISSION_TYPE, DATA_PERMISSION_DEPTID " +
                     "FROM SYS_ROLE SR, SYS_ROLE_USER SRU " +
                     "WHERE SR.ID = SRU.ROLE_ID  and SR.LOCKED != '0001' AND USER_ID=?";
-            return this.jdbcTemplate.queryForList(sql, userId );
-        }catch (EmptyResultDataAccessException e){
+            return this.jdbcTemplate.queryForList(sql, userId);
+        } catch (EmptyResultDataAccessException e) {
             return Collections.emptyList();
         }
     }
+
     /**
      * 根据角色名称查询其拥有的资源路径
      */
-    @Cacheable(cacheNames = CommonConstant.CACHE_SYSDATA, key = "'"+ CommonConstant.REDIS_ROLES_PERMISSION_PREFIX_KEY + "'" + "+#roleId")
-    public List<String>  listRoleNamesWithPath(String roleId){
-        try{
-            String sql="SELECT REPLACE(CONCAT(IFNULL(GROUP_CONCAT(B.PATH ORDER BY FIND_IN_SET( B.ID, A.PATH_ID)),''),',',ANY_VALUE(A.PATH)),',','') AS PATH " +
+    @Cacheable(cacheNames = CommonConstant.CACHE_SYSDATA, key = "'" + CommonConstant.REDIS_ROLES_PERMISSION_PREFIX_KEY + "'" + "+#roleId")
+    public List<String> listRoleNamesWithPath(String roleId) {
+        try {
+            String sql = "SELECT REPLACE(CONCAT(IFNULL(GROUP_CONCAT(B.PATH ORDER BY FIND_IN_SET( B.ID, A.PATH_ID)),''),',',ANY_VALUE(A.PATH)),',','') AS PATH " +
                     "FROM ( " +
                     "SELECT DISTINCT  CONCAT(M.ID,',',MR.ID) ID,M.PATH_ID, REPLACE(CONCAT(M.PATH,'/',MR.RESOURCE_URL),'//','/') PATH " +
                     "FROM SYS_MODULE_RESOURCES MR, SYS_ROLE_MODULE RM, SYS_MODULE M, SYS_ROLE R " +
@@ -93,7 +96,7 @@ public class JDBCService {
                     "LEFT JOIN SYS_MODULE B ON FIND_IN_SET( B.ID, A.PATH_ID) " +
                     "GROUP BY A.ID";
             return this.jdbcTemplate.queryForList(sql, String.class, roleId);
-        }catch (EmptyResultDataAccessException e){
+        } catch (EmptyResultDataAccessException e) {
             return Collections.emptyList();
         }
     }
