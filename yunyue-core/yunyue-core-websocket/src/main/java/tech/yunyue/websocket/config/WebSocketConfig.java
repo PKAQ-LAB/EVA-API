584a26faa6bde1e44c644e2ca7214c5a756017e6
Adameve <1135656594@qq.com>
2024-01-04 15:48:02 +0800

    判空

--- yunyue-core/yunyue-core-websocket/src/main/java/tech/yunyue/websocket/config/WebSocketConfig.java
+++ yunyue-core/yunyue-core-websocket/src/main/java/tech/yunyue/websocket/config/WebSocketConfig.java
@@ -1,8 +1,8 @@
 package tech.yunyue.websocket.config;
 
 import cn.hutool.core.collection.CollUtil;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
@@ -20,11 +20,11 @@ import java.util.Optional;
  */
 @Configuration
 @EnableWebSocket
-@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketConfigurer {
-    @Autowired
+    @Autowired(required = false)
     private List<WebSocketHandler> socketHandlers;
-    @Autowired
+    @Autowired(required = false)
+    @Qualifier("webSocketAuthInterceptor")
     private HandshakeInterceptor interceptor;
 
     @Bean
