d2b91fb725115a988fde51543e3addf7391c38c9
Adameve <1135656594@qq.com>
2024-01-04 16:19:39 +0800

    删除无用代码，无需新建websocket鉴权类，socket链接会走原JwtFilter鉴权逻辑

--- yunyue-core/yunyue-core-websocket/src/main/java/tech/yunyue/websocket/config/WebSocketConfig.java
+++ yunyue-core/yunyue-core-websocket/src/main/java/tech/yunyue/websocket/config/WebSocketConfig.java
@@ -2,18 +2,15 @@ package tech.yunyue.websocket.config;
 
 import cn.hutool.core.collection.CollUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-import org.springframework.web.socket.server.HandshakeInterceptor;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 import tech.yunyue.websocket.handler.WebSocketHandler;
 
 import java.util.List;
-import java.util.Optional;
 
 /**
  * @author PKAQ
@@ -23,9 +20,6 @@ import java.util.Optional;
 public class WebSocketConfig implements WebSocketConfigurer {
     @Autowired(required = false)
     private List<WebSocketHandler> socketHandlers;
-    @Autowired(required = false)
-    @Qualifier("webSocketAuthInterceptor")
-    private HandshakeInterceptor interceptor;
 
     @Bean
     public ServerEndpointExporter serverEndpointExporter() {
@@ -37,7 +31,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
         if (CollUtil.isNotEmpty(socketHandlers)) {
             socketHandlers.forEach(handler -> {
                 registry.addHandler(handler, handler.socketPath())
-                        .addInterceptors(Optional.ofNullable(handler.getInterceptor()).orElse(interceptor))
+                        .addInterceptors(handler.getInterceptor())
                         // 允许跨域，方便本地调试，生产建议去掉
                         .setAllowedOrigins("*");
                 //.setAllowedOriginPatterns("*")
