a1a76477956fbb456cb330a749fa25f8960d811d
Adameve <1135656594@qq.com>
2024-01-12 15:13:18 +0800

    黑名单service实现CommandLineRunner，将黑名单列表加入到缓存中

--- yunyue-web/yunyue-web-sa-auth/src/main/java/tech/yunyue/blacklist/service/BlackListService.java
+++ yunyue-web/yunyue-web-sa-auth/src/main/java/tech/yunyue/blacklist/service/BlackListService.java
@@ -1,16 +1,16 @@
 package tech.yunyue.blacklist.service;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
-import tech.yunyue.core.log.annotation.BizLog;
-import tech.yunyue.core.log.base.BizLogEnum;
-import tech.yunyue.core.mybatis.mvc.service.mybatis.StdService;
 import tech.yunyue.blacklist.cache.BlackListCacheHelper;
 import tech.yunyue.blacklist.entity.BlackListEntity;
 import tech.yunyue.blacklist.mapper.BlackListMapper;
+import tech.yunyue.core.log.annotation.BizLog;
+import tech.yunyue.core.log.base.BizLogEnum;
+import tech.yunyue.core.mybatis.mvc.service.mybatis.StdService;
 
 import java.util.ArrayList;
 import java.util.stream.Collectors;
@@ -21,37 +21,41 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 @Schema(description = "黑名单管理")
-public class BlackListService extends StdService<BlackListMapper, BlackListEntity> {
+public class BlackListService extends StdService<BlackListMapper, BlackListEntity> implements CommandLineRunner {
     private final BlackListCacheHelper blackListCacheHelper;
 
     /**
      * 初始化黑名单数据缓存  启动项目则加载到缓存
      */
-    @PostConstruct
     public void init() {
         var blackList = this.list(null).stream().map(BlackListEntity::getTarget).collect(Collectors.toList());
         blackListCacheHelper.cachePut(blackList);
     }
 
-    @BizLog(operateType= BizLogEnum.CREATE_UPDATE,description = "保存黑名单列表[{0}]",args = {"param:0.id"})
+    @Override
+    public void run(String... args) {
+        this.init();
+    }
+
+    @BizLog(operateType = BizLogEnum.CREATE_UPDATE, description = "保存黑名单列表[{0}]", args = {"param:0.id"})
     @Override
     public void merge(BlackListEntity entity) {
-        //新增/修改缓存
+        // 新增/修改缓存
         if (!StringUtils.hasText(entity.getId())) {
             blackListCacheHelper.add(entity.getTarget());
         } else {
             BlackListEntity oldEntity = this.getById(entity.getId());
-            blackListCacheHelper.add(oldEntity.getTarget(),entity.getTarget());
+            blackListCacheHelper.add(oldEntity.getTarget(), entity.getTarget());
         }
 
         super.merge(entity);
     }
 
-    @BizLog(operateType= BizLogEnum.DELETE,description = "删除黑名单列表[{0}]",args = {"param:0"})
+    @BizLog(operateType = BizLogEnum.DELETE, description = "删除黑名单列表[{0}]", args = {"param:0"})
     @Override
     public void delete(ArrayList<String> param) {
         super.delete(param);
-        //删除完成后重新加载缓存
+        // 删除完成后重新加载缓存
         this.init();
     }
 }
