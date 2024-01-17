a7c4540067a45bf9ccdd9af4f71ffee99f0c4054
Adameve <1135656594@qq.com>
2024-01-17 19:31:17 +0800

    根据日期查询日志处理 开始日期为00:00 结束日期为59:59

--- yunyue-core/yunyue-core-log/src/main/java/tech/yunyue/core/log/base/bo/LogQueryBo.java
+++ yunyue-core/yunyue-core-log/src/main/java/tech/yunyue/core/log/base/bo/LogQueryBo.java
@@ -1,5 +1,7 @@
 package tech.yunyue.core.log.base.bo;
 
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.date.DateUtil;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -7,6 +9,7 @@ import tech.yunyue.core.log.base.LogEntity;
 import tech.yunyue.core.mvc.vo.PageBo;
 
 import java.util.Date;
+import java.util.Optional;
 
 @Schema(description = "日志列表请求参数")
 @Data
@@ -14,11 +17,30 @@ import java.util.Date;
 public class LogQueryBo<T extends LogEntity> extends PageBo {
 
     @Schema(description = "开始时间")
-    private Date begin;
+    private DateTime begin;
 
     @Schema(description = "结束时间")
-    private Date end;
+    private DateTime end;
 
     private T logEntity;
 
+    public void setEnd(DateTime end) {
+        this.end = end;
+    }
+
+    public void setBegin(DateTime begin) {
+        this.begin = begin;
+    }
+
+    public void setEnd(Date end) {
+        Optional.ofNullable(end).ifPresent(e -> {
+            this.end = DateUtil.endOfDay(end);
+        });
+    }
+
+    public void setBegin(Date begin) {
+        Optional.ofNullable(begin).ifPresent(e -> {
+            this.begin = DateUtil.beginOfDay(begin);
+        });
+    }
 }
