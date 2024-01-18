b0ac386541fadc2f6aaa7cb5713a474e10abc795
Adameve <1135656594@qq.com>
2024-01-18 10:46:23 +0800

    资源鉴权匹配路径使用AntPathMatcher

--- yunyue-web/yunyue-web-sa-auth/src/main/java/tech/yunyue/config/FilterAuthStrategy.java
+++ yunyue-web/yunyue-web-sa-auth/src/main/java/tech/yunyue/config/FilterAuthStrategy.java
@@ -2,16 +2,19 @@ package tech.yunyue.config;
 
 import cn.dev33.satoken.context.SaHolder;
 import cn.dev33.satoken.filter.SaFilterAuthStrategy;
+import cn.dev33.satoken.fun.SaParamRetFunction;
 import cn.dev33.satoken.router.SaRouter;
 import cn.dev33.satoken.stp.StpUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.util.StringUtils;
 import tech.yunyue.core.enums.BizCodeEnum;
 import tech.yunyue.core.properties.EvaConfig;
 import tech.yunyue.core.threaduser.ThreadUserHelper;
 
+import java.util.Arrays;
 import java.util.Objects;
 
 /**
@@ -24,24 +27,36 @@ public class FilterAuthStrategy implements SaFilterAuthStrategy {
 
     @Override
     public void run(Object o) {
-        //排除可以匿名访问的接口
+        // 排除可以匿名访问的接口
         SaRouter.match(evaConfig.getSecurity().getAnonymous()).stop();
         //  SaRouter.match("/**", "/auth/login", r -> StpUtil.checkLogin()); login进来为false 别的进来都是true
 
-        //鉴权
-        SaRouter.match("/**",r->{
+        // 鉴权
+        SaRouter.match("/**", r -> {
             // 当前会话是否登录
-            if(Objects.isNull(ThreadUserHelper.getCurrentUser()) || !StringUtils.hasText(ThreadUserHelper.getUserId())){
+            if (Objects.isNull(ThreadUserHelper.getCurrentUser()) || !StringUtils.hasText(ThreadUserHelper.getUserId())) {
                 SaHolder.getResponse().setStatus(HttpStatus.UNAUTHORIZED.value());
                 BizCodeEnum.LOGIN_EXPIRED.newException();
             }
-            //当前资源是否需要鉴权
-            SaRouter.match(evaConfig.getSecurity().getPermit()).stop();
-            //权限验证
+            // 当前资源是否需要鉴权
+            SaRouter.match(antPathMatcher()).stop();
+            // 权限验证
             if (evaConfig.getResourcePermission().isEnable() && !StpUtil.hasPermission(ThreadUserHelper.getUserId(), SaHolder.getRequest().getRequestPath())) {
                 BizCodeEnum.PERMISSION_DENY.newException();
             }
         });
 
+    }
+
+    private SaParamRetFunction<Object, Boolean> antPathMatcher() {
+        return param -> {
+            var patterns = evaConfig.getSecurity().getPermit();
+            if (patterns == null) {
+                return false;
+            }
+            AntPathMatcher pathMatcher = new AntPathMatcher();
+            var path = SaHolder.getRequest().getRequestPath();
+            return Arrays.stream(patterns).anyMatch(pattern -> pathMatcher.match(pattern, path));
+        };
     }
 }
