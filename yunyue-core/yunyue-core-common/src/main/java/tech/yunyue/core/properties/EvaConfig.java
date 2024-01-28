a1042490e38a0f4734a247e70ba5054bab7d57d9
Adameve <1135656594@qq.com>
2024-01-28 11:48:06 +0800

    删除无用注解

--- yunyue-core/yunyue-core-common/src/main/java/tech/yunyue/core/properties/EvaConfig.java
+++ yunyue-core/yunyue-core-common/src/main/java/tech/yunyue/core/properties/EvaConfig.java
@@ -2,7 +2,6 @@ package tech.yunyue.core.properties;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Optional;
@@ -15,7 +14,6 @@ import java.util.Optional;
 @Data
 @Configuration
 @ConfigurationProperties(prefix = "eva")
-@EnableConfigurationProperties(EvaConfig.class)
 public class EvaConfig {
     /**
      * 是否开启国际化
