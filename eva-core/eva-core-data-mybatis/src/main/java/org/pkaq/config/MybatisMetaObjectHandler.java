d1cd588c728405d268beea610ccfc8a79b51d810
PKAQ <pkaq@msn.com>
2025-05-07 15:13:26 +0800

    完善自动填充设置

--- eva-core/eva-core-data-mybatis/src/main/java/org/pkaq/config/MybatisMetaObjectHandler.java
+++ eva-core/eva-core-data-mybatis/src/main/java/org/pkaq/config/MybatisMetaObjectHandler.java
@@ -7,6 +7,8 @@ import org.pkaq.core.threaduser.ThreadUserHelper;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.function.Supplier;
 
 /**
  * Mybatis Plus 自动填充策略实现类
@@ -20,17 +22,31 @@ public class MybatisMetaObjectHandler implements MetaObjectHandler {
     @Override
     public void insertFill(MetaObject metaObject) {
         log.debug("start insert fill ....");
-        this.strictInsertFill(metaObject, "createBy", String.class, ThreadUserHelper.getUserId());
-        this.strictInsertFill(metaObject, "modifyBy", String.class, ThreadUserHelper.getUserId());
-        this.strictInsertFill(metaObject, "gmtCreate", LocalDateTime.class, LocalDateTime.now());
-        this.strictInsertFill(metaObject, "gmtModify", LocalDateTime.class, LocalDateTime.now());
-
+        this.strictInsertFill(metaObject, "createId", String.class, ThreadUserHelper.getUserId());
+        this.strictInsertFill(metaObject, "createBy", String.class, ThreadUserHelper.getUserName());
+        this.strictInsertFill(metaObject, "utcCreate", LocalDateTime.class, LocalDateTime.now());
+        this.strictInsertFill(metaObject, "sort", Integer.class, 0);
+        this.strictInsertFill(metaObject, "frozen", String.class, "0000");
+        this.strictInsertFill(metaObject, "deleted", String.class, "0");
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
         log.debug("start update fill ....");
-        this.strictUpdateFill(metaObject, "modifyBy", String.class, ThreadUserHelper.getUserId());
-        this.strictUpdateFill(metaObject, "gmtModify", LocalDateTime.class, LocalDateTime.now());
+        this.strictInsertFill(metaObject, "modifyId", String.class, ThreadUserHelper.getUserId());
+        this.strictUpdateFill(metaObject, "modifyBy", String.class, ThreadUserHelper.getUserName());
+        this.strictUpdateFill(metaObject, "utcModify", LocalDateTime.class, LocalDateTime.now());
+    }
+
+    /**
+     * 不管是否有值 都填充
+     */
+    @Override
+    public MetaObjectHandler strictFillStrategy(MetaObject metaObject, String fieldName, Supplier<?> fieldVal) {
+        Object obj = fieldVal.get();
+        if (Objects.nonNull(obj)) {
+            metaObject.setValue(fieldName, obj);
+        }
+        return this;
     }
 }
