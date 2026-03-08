5e8873f8c99539d93723ce6fa904e5f2a329cc02
PKAQ <pkaq@msn.com>
2026-03-08 17:54:29 +0800

    让常量类更合理

--- eva-core/eva-core-common/src/main/java/org/pkaq/core/constant/CommonConstant.java
+++ eva-core/eva-core-common/src/main/java/org/pkaq/core/constant/CommonConstant.java
@@ -1,67 +1,72 @@
 package org.pkaq.core.constant;
 
 
+import lombok.experimental.UtilityClass;
+
 /**
  * 系统内置常量
  *
  * @author PKAQ
  */
-public interface CommonConstant {
+@UtilityClass
+public class CommonConstant {
 
-    String AUTH_PREFIX = "ROLE_";
+    public final static String AUTH_PREFIX = "ROLE_";
+    public final static String MODE_SINGLETON = "singleton";
+    public final static String MODE_SAAS = "saas";
     /**
      * 有效记录标识
      */
-    String EFFECTIVE_RECORD = "0000";
+    public final static String EFFECTIVE_RECORD = "0000";
     // 默认分页条数
     Integer PAGE_SIZE = 10;
-    String ADMIN_ROLE_NAME = "ROLE_ADMIN";
+    public final static String ADMIN_ROLE_NAME = "ROLE_ADMIN";
     // 鉴权字符串常量
-    String TOKEN_KEY = "auth_token";
-    String ACCESS_TOKEN_KEY = "access_token";
-    String REFRESH_TOKEN_KEY = "refresh_token";
+    public final static String TOKEN_KEY = "auth_token";
+    public final static String ACCESS_TOKEN_KEY = "access_token";
+    public final static String REFRESH_TOKEN_KEY = "refresh_token";
     // 网关转发后下发的用户信息请求头
-    String X_CLIENT_TOKEN_USER = "x-client-token-user";
+    public final static String X_CLIENT_TOKEN_USER = "x-client-token-user";
     // 网关转发后下发的权限信息请求头
-    String X_CLIENT_TOKEN_ROLES = "x-client-token-roles";
+    public final static String X_CLIENT_TOKEN_ROLES = "x-client-token-roles";
     // 网关加入的请求头
-    String X_GATEWAY_HEADER = "x-request";
+    public final static String X_GATEWAY_HEADER = "x-request";
     // 网关加入的请求串
-    String X_GATEWAY_VALUE = "eva-gateway-request";
+    public final static String X_GATEWAY_VALUE = "eva-gateway-request";
     // JWT 存储用户id的字符串
-    String JWT_USER_ID_STR = "userId";
+    public final static String JWT_USER_ID_STR = "userId";
     // JWT 存储用户name的字符串
-    String JWT_USER_NAME_STR = "userName";
+    public final static String JWT_USER_NAME_STR = "userName";
     // JWT 存储权限的字符串
-    String JWT_USER_ROLES_STR = "authorities";
+    public final static String JWT_USER_ROLES_STR = "authorities";
 
-    String BLOCK_PREFIX = "JWTBLOCK:";
+    public final static String BLOCK_PREFIX = "JWTBLOCK:";
     // 用户信息字符串常量
-    String USER_KEY = "user_info";
+    public final static String USER_KEY = "user_info";
     // 文件缓存前缀
-    String FILE_CACHE_PREFIX = "FILE_TMP_";
+    public final static String FILE_CACHE_PREFIX = "FILE_TMP_";
     // cachename
     // 文件上传
-    String CACHE_UPLOADFILES = "uploadfiles";
+    public final static String CACHE_UPLOADFILES = "uploadfiles";
     // 重复提交
-    String CACHE_REPEATSUBMIT = "repeatsubmit";
+    public final static String CACHE_REPEATSUBMIT = "repeatsubmit";
     //业务数据
-    String CACHE_BIZDATA = "bizdata";
+    public final static String CACHE_BIZDATA = "bizdata";
     //字典数据
-    String CACHE_DICTDATA = "dictdata";
+    public final static String CACHE_DICTDATA = "dictdata";
     // token缓存
-    String CACHE_TOKEN = "token";
+    public final static String CACHE_TOKEN = "token";
     //业务用
-    String UNKNOWN = "unknown";
+    public final static String UNKNOWN = "unknown";
     // 请求来源设备类型
-    String DEVICE = "device";
+    public final static String DEVICE = "device";
     // 请求来源版本号
-    String VERSION = "version";
+    public final static String VERSION = "version";
 
     // 用户操作的模块id请求头
-    String MODULE_ID = "mid";
+    public final static String MODULE_ID = "mid";
     // 用户操作的模块code请求头
-    String MODULE_CODE = "mcode";
+    public final static String MODULE_CODE = "mcode";
 
-    String SYS_ALL_DICT_KEY = "'sys:all:dict'";
+    public final static String SYS_ALL_DICT_KEY = "'sys:all:dict'";
 }
